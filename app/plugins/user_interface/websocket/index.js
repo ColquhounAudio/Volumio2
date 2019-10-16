e23e142f16febc6c689af5d9436a8a738f5f7452
root <ggiraudon@prism19.com>
2019-10-16 19:34:26 +0000

    Adding IR Remote capability
    # Conflicts:
    #	app/mainmenu.json

--- app/plugins/user_interface/websocket/index.js
+++ app/plugins/user_interface/websocket/index.js
@@ -907,7 +907,6 @@ function InterfaceWebUI(context) {
 
 
 
-
 		connWebSocket.on('factoryReset', function () {
 			var selfConnWebSocket = this;
 			self.logger.info("Command Factory Reset Received");
